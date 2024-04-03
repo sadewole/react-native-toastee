315754e5764c6d685d0421251412e50b23635129
sadewole <samueladewole15@gmail.com>
2024-04-03 19:45:25 +0100

    feat: create react-native-toastify

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,19 +1,42 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-toastify';
+import { StyleSheet, View, Button, Switch, Text } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ToastProvider, toast } from 'react-native-toastify';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [isTopPosition, setIsTopPosition] = React.useState(true);
+  const handleToast = () => {
+    toast('Hello from react-native-toastify');
+  };
+  const handleSuccessToast = () =>
+    toast('Hello from react-native-toastify', 'success');
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+  const handleFailedToast = () =>
+    toast('Hello from react-native-toastify', 'failure');
 
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
+    <SafeAreaProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <View style={styles.container}>
+          <View style={{ flexDirection: 'row', gap: 16, alignItems: 'center' }}>
+            <Text>Top Position: </Text>
+            <Switch
+              trackColor={{ false: '#393939', true: '#00AD50' }}
+              thumbColor="#fff"
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={() => setIsTopPosition(!isTopPosition)}
+              value={isTopPosition}
+            />
+          </View>
+          <Button title="Default toast" onPress={handleToast} />
+          <Button title="Success toast" onPress={handleSuccessToast} />
+          <Button title="Error toast" onPress={handleFailedToast} />
+        </View>
+        <ToastProvider position={isTopPosition ? 'top' : 'bottom'} />
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   );
 }
 
